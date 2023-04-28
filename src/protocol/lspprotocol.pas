736500da5d89176d9e05f880d4f45136a3361e07
Michael VAN CANNEYT <mvancanneyt@tixeo.com>
2023-04-28 19:15:00 +0200

    * Rename InlayHint

--- src/protocol/lspprotocol.pas
+++ src/protocol/lspprotocol.pas
@@ -11,7 +11,7 @@ uses
   LSP.CodeAction, LSP.Bridge.CodeUtils, LSP.Bridge.Commands, LSP.Diagnostics, 
   LSP.DocumentHighlight, LSP.DocumentSymbol, LSP.ExecuteCommand, 
   LSP.GotoDeclaration, LSP.GotoDefinition, LSP.GotoImplementation, LSP.Hover, 
-  LSP.Inlayhint, MemUtils, LSP.Basic, LSP.Capabilities, LSP.Completion, 
+  LSP.InlayHint, MemUtils, LSP.Basic, LSP.Capabilities, LSP.Completion, 
   LSP.General, LSP.Base, LSP.Options, LSP.References, LSP.Settings, 
   LSP.SignatureHelp, LSP.Symbols, LSP.Synchronization, LSP.Window, 
   LSP.WorkDoneProgress, LSP.Workspace, LSP.Bridge.LazConfig, LazarusPackageIntf;
